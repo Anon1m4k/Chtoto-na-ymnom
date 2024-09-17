79b1410604b8742001355928910f71664b9c5058
Anon1m4k <kirilvalera2@gmail.com>
2024-09-17 13:10:46 +0300

    Я решил первую задачу 3-его варианта

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int> { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            for (int i = -5; i < 10; i++)
+            {
+               list.Remove(i);               
+            }
+
+            for (int i = 0; i <list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadKey();
+
         }
     }
 }
