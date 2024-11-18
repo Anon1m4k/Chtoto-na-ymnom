da73ed85141bd494060770b587be17f184525e90
Anon1m4k <kirilvalera2@gmail.com>
2024-11-18 17:51:20 +0300

    Сделал дз

--- Какая-то домашняя работа/Program.cs
+++ Какая-то домашняя работа/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Какая_то_домашняя_работа
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<string> groups = new List<string>() { "П-10", "П-21", "П-40", "П-32", "Ю-11", "Ю-20", "Ю-30", "Б-10", "Б-20", "Ю-40" }; 
+           
+            Console.WriteLine("Введите номер курса: "); 
+            string input = Console.ReadLine();
+            
+            foreach (string group in groups)
+            {
+                if (group.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    Console.WriteLine(String.Join(",", group));
+                }
+                else
+                {
+                    Console.WriteLine("Нет такого курса!");
+                    Console.ReadKey();
+                }                
+            }           
+            
         }
     }
 }
