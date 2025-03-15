b9cf50c051fb66897a51206d1a560401648783d2
Anon1m4k <kirilvalera2@gmail.com>
2025-03-15 09:24:13 +0300

    новый проект добавление, редактирование

---
+++ exsempl/AddStusentForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace exsempl
+{
+    partial class AddStusentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxGroup = new System.Windows.Forms.TextBox();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            this.КОк = new System.Windows.Forms.Button();
+            this.КОтмена = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(94, 70);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxGroup
+            // 
+            this.textBoxGroup.Location = new System.Drawing.Point(94, 136);
+            this.textBoxGroup.Name = "textBoxGroup";
+            this.textBoxGroup.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGroup.TabIndex = 1;
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(94, 207);
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownAge.TabIndex = 3;
+            // 
+            // КОк
+            // 
+            this.КОк.Location = new System.Drawing.Point(94, 293);
+            this.КОк.Name = "КОк";
+            this.КОк.Size = new System.Drawing.Size(75, 23);
+            this.КОк.TabIndex = 4;
+            this.КОк.Text = "Ок";
+            this.КОк.UseVisualStyleBackColor = true;
+            // 
+            // КОтмена
+            // 
+            this.КОтмена.Location = new System.Drawing.Point(197, 293);
+            this.КОтмена.Name = "КОтмена";
+            this.КОтмена.Size = new System.Drawing.Size(75, 23);
+            this.КОтмена.TabIndex = 5;
+            this.КОтмена.Text = "Отмена";
+            this.КОтмена.UseVisualStyleBackColor = true;
+            // 
+            // AddStusentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.КОтмена);
+            this.Controls.Add(this.КОк);
+            this.Controls.Add(this.numericUpDownAge);
+            this.Controls.Add(this.textBoxGroup);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "AddStusentForm";
+            this.Text = "AddStusentForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxGroup;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+        private System.Windows.Forms.Button КОк;
+        private System.Windows.Forms.Button КОтмена;
+    }
+}
---
+++ exsempl/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace exsempl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.КДобавить = new System.Windows.Forms.Button();
+            this.КУдалить = new System.Windows.Forms.Button();
+            this.КРедактировать = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(220, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // КДобавить
+            // 
+            this.КДобавить.Location = new System.Drawing.Point(220, 227);
+            this.КДобавить.Name = "КДобавить";
+            this.КДобавить.Size = new System.Drawing.Size(75, 23);
+            this.КДобавить.TabIndex = 1;
+            this.КДобавить.Text = "Добавить";
+            this.КДобавить.UseVisualStyleBackColor = true;
+            // 
+            // КУдалить
+            // 
+            this.КУдалить.Location = new System.Drawing.Point(385, 227);
+            this.КУдалить.Name = "КУдалить";
+            this.КУдалить.Size = new System.Drawing.Size(75, 23);
+            this.КУдалить.TabIndex = 2;
+            this.КУдалить.Text = "Удалить";
+            this.КУдалить.UseVisualStyleBackColor = true;
+            // 
+            // КРедактировать
+            // 
+            this.КРедактировать.Location = new System.Drawing.Point(295, 279);
+            this.КРедактировать.Name = "КРедактировать";
+            this.КРедактировать.Size = new System.Drawing.Size(93, 23);
+            this.КРедактировать.TabIndex = 3;
+            this.КРедактировать.Text = "Редактировать";
+            this.КРедактировать.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.КРедактировать);
+            this.Controls.Add(this.КУдалить);
+            this.Controls.Add(this.КДобавить);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button КДобавить;
+        private System.Windows.Forms.Button КУдалить;
+        private System.Windows.Forms.Button КРедактировать;
+    }
+}
+
