d946a298d8fbbc8597f5d23f8f0ce7b60b68fbb7
Anon1m4k <kirilvalera2@gmail.com>
2025-03-15 09:34:40 +0300

    Добавил новых студентов

--- exsempl/Form1.cs
+++ exsempl/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,14 @@ namespace exsempl
 {
     public partial class Form1: Form
     {
+        BindingList<Student> B = new BindingList<Student>();
         public Form1()
         {
             InitializeComponent();
+            B.Add(new Student { Name = "ААА", Group = "П-10", Age = 18 });
+            B.Add(new Student { Name = "БББ", Group = "П-20", Age = 19 });
+            B.Add(new Student { Name = "ВВВ", Group = "П-30", Age = 20 });
         }
+        
     }
 }
